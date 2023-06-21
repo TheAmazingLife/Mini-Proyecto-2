0a17e516b4b81180326286b9dd23f5905fa60eb8
Joaquin San Martin <joaquin.san.vargas19@gmail.com>
2023-06-21 18:35:02 -0400

    Main con metodo de testeo insercion
    Co-Authored-By: Pablo Sanhueza <88067566+PabloSanhueza1@users.noreply.github.com>

--- QuadTree/main.cpp
+++ QuadTree/main.cpp
@@ -6,32 +6,39 @@
 #include "include/Quadtree.hpp"
 
 // Función auxiliar para reemplazar la coma por un punto en una cadena
-std::string replaceCommaWithDot(const std::string& str) {
+std::string replaceCommaWithDot(const std::string &str)
+{
     std::string result = str;
     std::size_t commaPos = result.find(',');
-    if (commaPos != std::string::npos) {
+    if (commaPos != std::string::npos)
+    {
         result.replace(commaPos, 1, ".");
     }
     return result;
 }
 
 // Función para formatear la entrada en un formato admitido por insert
-std::vector<std::tuple<Point, std::string, int>> formatData(const std::string& input) {
+std::vector<std::tuple<Point, std::string, int>> formatData(const std::string &input)
+{
     std::vector<std::tuple<Point, std::string, int>> formattedData;
 
     std::istringstream iss(input);
     std::string line;
     std::getline(iss, line); // Ignorar la primera línea (encabezado)
 
-    while (std::getline(iss, line)) {
+    while (std::getline(iss, line))
+    {
         std::istringstream lineStream(line);
         std::string field;
         std::vector<std::string> fields;
-        while (std::getline(lineStream, field, ';')) {
+
+        while (std::getline(lineStream, field, ';'))
+        {
             fields.push_back(field);
         }
 
-        if (fields.size() >= 8) {
+        if (fields.size() >= 8)
+        {
             double latitude = std::stod(replaceCommaWithDot(fields[5]));
             double longitude = std::stod(replaceCommaWithDot(fields[6]));
             int population = std::stoi(fields[4]);
@@ -45,9 +52,35 @@ std::vector<std::tuple<Point, std::string, int>> formatData(const std::string& i
 
     return formattedData;
 }
+/*
+void insertTest(Quadtree *quadtree, int n)
+{
+    auto start = chrono::high_resolution_clock::now();
 
+    int count = 0;
+    for (const auto &data : formattedData)
+    {
+        while (count < n)
+        {
+            Point point = std::get<0>(data);
+            std::string cityName = std::get<1>(data);
+            int population = std::get<2>(data);
+            quadtree.insert(point, cityName, population);
+            n++;
+        }
+    }
 
-int main() {
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> elapsedTime = end - start;
+
+    cout << "Tiempo total: " << elapsedTime.count() << " ms" << endl;
+    double tiempo_promedio = static_cast<double>(elapsedTime.count()) / n;
+    cout << "Tiempo promedio: " << fixed << setprecision(6) << tiempo_promedio << " ms" << endl << endl;
+}
+*/
+
+int main()
+{
     // Leer datos desde el archivo de entrada
     std::ifstream inputFile("../DataSet/worldcitiespop_fixed.csv");
     std::stringstream buffer;
@@ -59,7 +92,20 @@ int main() {
 
     // Crear el Quadtree y cargar los datos
     Quadtree quadtree;
-    for (const auto& data : formattedData) {
+
+    // vector<int> inputSizes = {100000, 200000, 400000, 800000, 1600000, 3173647};
+
+    /*
+    for (int size : inputSizes)
+    {
+        cout << "--------- Experimento ---------" << endl << endl;
+        cout << "size: " << size << endl << endl;
+        cout << "insert:" << endl;
+        insertTest(&quadtree, size);
+    }
+    */
+    for (const auto &data : formattedData)
+    {
         Point point = std::get<0>(data);
         std::string cityName = std::get<1>(data);
         int population = std::get<2>(data);
@@ -72,7 +118,7 @@ int main() {
 
     // Obtener la cantidad total de nodos
     int totalNodes = quadtree.totalNodes();
-    std::cout << "Cantidad total de nodos: " << totalNodes << std::endl;    
+    std::cout << "Cantidad total de nodos: " << totalNodes << std::endl;
 
     // Contar la cantidad de puntos en una región
     Point center = {40.920404, 39.19209};
@@ -101,7 +147,8 @@ int main() {
     // Obtener la lista de puntos almacenados en el Quadtree
     std::list<Point> pointList = quadtree.getPointList();
     std::cout << "Lista de puntos almacenados en el Quadtree:" << std::endl;
-    for (const auto& point : pointList) {
+    for (const auto &point : pointList)
+    {
         std::cout << "Longitud: " << point.longitude << ", Latitud: " << point.latitude << std::endl;
     }
 
